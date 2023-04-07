6b8ccd28aad116fbad408db30e7e7169a477d345
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-04-07 16:23:24 -0400

    catalyst update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -39,7 +39,7 @@ const Dashboard: React.FC = () => {
         />
       </VStack> */}
       <VStack spacing="5">
-        <Heading>Register for Horizons!</Heading>
+        <Heading>Register for Catalyst!</Heading>
         <a href="https://registration.hexlabs.org">
           <Button as={Button} align="center">
             Go!
